747fc02ae78aa0421f9f60763bb0151be778c59f
fahim32446 <azmirhasan.az2@gmail.com>
2023-04-24 10:40:07 +0600

    Fix Login

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -1,6 +1,13 @@
 const User = require('../models/User.js')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
-export const register = async (req, res) => {
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
+
+const register = async (req, res) => {
     const { name, email, password } = req.body;
     try {
         const userDoc = await User.create({
@@ -14,36 +21,46 @@ export const register = async (req, res) => {
     }
 };
 
-export const login = async (req, res) => {
-    console.log("HIII");
-    const { email, password } = req.body;
-    const userDoc = await User.findOne({ email });
-    if (userDoc) {
-        const passOk = bcrypt.compareSync(password, userDoc.password);
-        if (passOk) {
-            jwt.sign(
-                {
-                    email: userDoc.email,
-                    id: userDoc._id,
-                },
-                jwtSecret,
-                {},
-                (err, token) => {
-                    if (err) throw err;
-                    res
-                        .cookie('token', token, {
-                            sameSite: 'none',
-                            secure: true,
-                            httpOnly: false,
-                        })
-                        .json(userDoc);
-                }
-            );
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userDoc = await User.findOne({ email });
+        if (userDoc) {
+            const passOk = bcrypt.compareSync(password, userDoc.password);
+            if (passOk) {
+                jwt.sign(
+                    {
+                        email: userDoc.email,
+                        id: userDoc._id,
+                    },
+                    jwtSecret,
+                    {},
+                    (err, token) => {
+                        if (err) throw err;
+                        res
+                            .cookie('token', token, {
+                                sameSite: 'none',
+                                secure: true,
+                                httpOnly: false,
+                            })
+                            .json(userDoc);
+                    }
+                );
+            } else {
+                res.status(422).json('pass not ok');
+            }
         } else {
-            res.status(422).json('pass not ok');
+            res.json('not found');
         }
-    } else {
-        res.json('not found');
+
+    } catch (error) {
+        console.log(error);
     }
+
 };
 
+
+module.exports = {
+    register,
+    login
+};
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,8 @@
-// import { login, register } from '../controller/auth';
-const {login, register} = require('../controller/auth.js')
-
+const { login, register } = require('../controller/auth.js');
 const express = require('express');
-
 const router = express.Router();
-
-
 
 router.post('/register', register);
 router.post('/login', login);
 
-export default router
+module.exports = router;
